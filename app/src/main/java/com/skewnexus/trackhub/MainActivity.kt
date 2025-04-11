00207f4216591ed9bfcde30c538ad51255535fec
GreenVenom77 <hosome2222@gmail.com>
2025-04-11 23:51:10 +0200

    Moved from one scaffold approach to scaffold for each screen and changed the nav animation

--- app/src/main/java/com/skewnexus/trackhub/MainActivity.kt
+++ app/src/main/java/com/skewnexus/trackhub/MainActivity.kt
@@ -1,38 +1,30 @@
 package com.skewnexus.trackhub
 
 import android.content.Context
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
-import com.greenvenom.core_navigation.data.NavigationType
 import com.greenvenom.core_ui.theme.AppTheme
 import com.greenvenom.core_navigation.data.repository.NavigationStateRepository
-import com.greenvenom.core_ui.components.FloatingButton
 import com.greenvenom.core_menu.data.AppPrefStateRepository
 import com.skewnexus.trackhub.navigation.AppNavHost
 import com.trackhub.feat_navigation.components.BottomNavigationBar
-import com.trackhub.feat_navigation.components.TopAppBar
-import com.trackhub.feat_navigation.data.DestinationHandler
+import com.greenvenom.core_ui.components.TopAppBar
 import com.trackhub.feat_navigation.routes.Screen
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.getKoin
 import org.koin.compose.koinInject
@@ -55,87 +47,26 @@ class MainActivity : AppCompatActivity() {
         setContent {
             val navigationRepository = koinInject<NavigationStateRepository>()
             val navigationState by navigationRepository.navigationState.collectAsStateWithLifecycle()
-
-            val destinationHandler = koinInject<DestinationHandler>()
-            val destinationState by destinationHandler.destinationState.collectAsStateWithLifecycle()
-
             val appPrefState by appPrefStateRepository.appPrefState.collectAsStateWithLifecycle()
 
             AppTheme(darkTheme = appPrefState.isDarkTheme) {
-                Scaffold(
-                    topBar = {
-                        TopAppBar(
-                            title = destinationState.currentHub?.name ?: stringResource(R.string.app_name),
-                            isVisible = navigationState.topBarState,
-                            isSideDestination = navigationState.isCurrentDestinationSide,
-                            navigateBack = {
-                                navigationRepository.navigate(NavigationType.Back)
-                                destinationHandler.destinationState.update {
-                                    it.copy(
-                                        currentHub = null
-                                    )
-                                }
-                            },
-                            isActionEnabled = navigationState.currentDestination is Screen.HubDetails,
-                            action = {
-                                if (navigationState.currentDestination is Screen.HubDetails) {
-                                    IconButton(
-                                        onClick = {
-                                            destinationHandler.destinationState.update {
-                                                it.copy(
-                                                    hubBottomSheetState = true
-                                                )
-                                            }
-                                        },
-                                        modifier = Modifier
-                                            .size(48.dp)
-                                            .padding(8.dp)
-                                    ) {
-                                        Icon(
-                                            painter = painterResource(R.drawable.edit_ic),
-                                            contentDescription = stringResource(R.string.edit_hub_Details),
-                                            modifier = Modifier.size(48.dp)
-                                        )
-                                    }
-                                }
-                            }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Column(modifier = Modifier.fillMaxSize()) {
+                        AppNavHost(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .weight(1f)
                         )
-                    },
-                    bottomBar = {
+
                         BottomNavigationBar(
                             defaultNavigationMethod = navigationRepository::navigate,
                             currentDestination = navigationState.currentDestination ?: Screen.MyHubs,
                             isVisible = navigationState.bottomBarState
                         )
-                    },
-                    floatingActionButton = {
-                        FloatingButton(
-                            isVisible = navigationState.currentDestination is Screen.HubDetails
-                                || navigationState.currentDestination is Screen.MyHubs,
-                            onClick = {
-                                destinationHandler.destinationState.update {
-                                    if (navigationState.currentDestination is Screen.HubDetails) {
-                                        it.copy(
-                                            itemBottomSheetState = true
-                                        )
-                                    } else {
-                                        it.copy(
-                                            hubBottomSheetState = true
-                                        )
-                                    }
-                                }
-                            },
-                            modifier = Modifier
-                                .size(64.dp)
-                        )
-                    },
-                    floatingActionButtonPosition = FabPosition.End
-                ) { innerPadding ->
-                    AppNavHost(
-                        modifier = Modifier
-                        .padding(innerPadding)
-                        .fillMaxSize()
-                    )
+                    }
                 }
             }
         }
--- hub/feat-hub/src/main/java/com/trackhub/feat_hub/presentation/hub_details/HubDetailsScreen.kt
+++ hub/feat-hub/src/main/java/com/trackhub/feat_hub/presentation/hub_details/HubDetailsScreen.kt
@@ -2,10 +2,15 @@ package com.trackhub.feat_hub.presentation.hub_details
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -13,21 +18,21 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleEventObserver
-import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.greenvenom.core_network.data.onError
 import com.greenvenom.core_network.data.onSuccess
 import com.greenvenom.core_network.utils.toString
+import com.greenvenom.core_ui.components.FloatingButton
+import com.greenvenom.core_ui.components.TopAppBar
 import com.greenvenom.core_ui.presentation.BaseAction
 import com.greenvenom.core_ui.presentation.BaseScreen
-import com.trackhub.core_hub.domain.models.Hub
 import com.trackhub.feat_hub.R
 import com.trackhub.feat_hub.presentation.components.HubBottomSheet
 import com.trackhub.feat_hub.presentation.components.ItemBottomSheet
@@ -39,18 +44,11 @@ import com.trackhub.feat_hub.presentation.models.toHubUI
 fun HubDetailsScreen(
     hubId: String,
     onPhysicalBack: () -> Unit,
-    onHubDeletion: () -> Unit,
-    onHubRetrieval: (Hub) -> Unit,
-    hubBottomSheetState: Boolean,
-    itemBottomSheetState: Boolean,
-    onSheetDismiss: () -> Unit,
-    onEditItem: () -> Unit,
     navigateBack: () -> Unit
 ) {
     BaseScreen<HubDetailsViewModel>(
         onStopAction = {
             onPhysicalBack()
-            onSheetDismiss()
         }
     ) { viewModel ->
         val hubDetailsState by viewModel.hubDetailsState.collectAsStateWithLifecycle()
@@ -68,18 +66,11 @@ fun HubDetailsScreen(
             hubDetailsState = hubDetailsState,
             hubDetailsAction = viewModel::hubDetailsAction,
             baseAction = viewModel::baseAction,
-            hubBottomSheetState = hubBottomSheetState,
-            itemBottomSheetState = itemBottomSheetState,
-            onSheetDismiss = onSheetDismiss,
-            onHubDeletion = onHubDeletion,
-            onEditItem = onEditItem,
             navigateBack = navigateBack,
             modifier = Modifier
                 .fillMaxSize()
                 .padding(12.dp)
         )
-
-        hubDetailsState.hub?.let { onHubRetrieval(it) }
     }
 }
 
@@ -89,11 +80,6 @@ private fun HubDetailsContent(
     hubDetailsState: HubDetailsState,
     hubDetailsAction: (HubDetailsAction) -> Unit,
     baseAction: (BaseAction) -> Unit,
-    hubBottomSheetState: Boolean,
-    itemBottomSheetState: Boolean,
-    onSheetDismiss: () -> Unit,
-    onHubDeletion: () -> Unit,
-    onEditItem: () -> Unit,
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -101,6 +87,8 @@ private fun HubDetailsContent(
     val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
     val hubItemsResult = hubDetailsState.hubItemsResult
     var isItemEdit by rememberSaveable { mutableStateOf(false) }
+    var hubSheetState by rememberSaveable { mutableStateOf(false) }
+    var itemSheetState by rememberSaveable { mutableStateOf(false) }
 
     hubItemsResult
         ?.onSuccess { baseAction(BaseAction.HideLoading) }
@@ -115,8 +103,8 @@ private fun HubDetailsContent(
         ?.onSuccess {
             baseAction(BaseAction.HideLoading)
             hubDetailsAction(HubDetailsAction.ClearNetworkOperations)
-            onSheetDismiss()
             hubDetailsAction(HubDetailsAction.UpdateCurrentItem(null))
+            itemSheetState = false
             isItemEdit = false
         }
         ?.onError { error ->
@@ -132,8 +120,8 @@ private fun HubDetailsContent(
     hubDetailsState.hubUpdateResult
         ?.onSuccess {
             baseAction(BaseAction.HideLoading)
+            hubSheetState = false
             hubDetailsAction(HubDetailsAction.ClearNetworkOperations)
-            onSheetDismiss()
             navigateBack()
         }
         ?.onError { error ->
@@ -150,8 +138,9 @@ private fun HubDetailsContent(
     hubDetailsState.hubDeletionResult
         ?.onSuccess {
             baseAction(BaseAction.HideLoading)
+            hubSheetState = false
             hubDetailsAction(HubDetailsAction.ClearNetworkOperations)
-            onHubDeletion()
+            navigateBack()
         }
         ?.onError { error ->
             baseAction(BaseAction.HideLoading)
@@ -166,8 +155,8 @@ private fun HubDetailsContent(
     hubDetailsState.itemDeletionResult
         ?.onSuccess {
             baseAction(BaseAction.HideLoading)
+            itemSheetState = false
             hubDetailsAction(HubDetailsAction.ClearNetworkOperations)
-            onSheetDismiss()
             hubDetailsAction(HubDetailsAction.UpdateCurrentItem(null))
             isItemEdit = false
         }
@@ -181,77 +170,122 @@ private fun HubDetailsContent(
             ))
         }
 
-    Box(modifier = modifier.fillMaxSize()) {
-        hubItemsResult?.onSuccess { hubItems ->
-            LazyColumn(modifier = Modifier.fillMaxSize()) {
-                items(
-                    items = hubItems,
-                    key = { hubItem -> hubItem.id }
-                ) { hubItem ->
-                    ItemListCard(
-                        hubItem = hubItem.toHubItemUI(),
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                isVisible = true,
+                isSideDestination = true,
+                title = hubDetailsState.hub?.name ?: stringResource(R.string.unknown_hub),
+                navigateBack = { navigateBack() },
+                isActionEnabled = true,
+                action = {
+                    IconButton(
                         onClick = {
-                            isItemEdit = true
-                            hubDetailsAction(HubDetailsAction.UpdateCurrentItem(hubItem))
-                            onEditItem()
+                            hubSheetState = true
                         },
                         modifier = Modifier
-                            .padding(4.dp)
+                            .size(48.dp)
+                            .padding(8.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.edit_ic),
+                            contentDescription = stringResource(R.string.edit_hub_Details),
+                            modifier = Modifier.size(48.dp)
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingButton(
+                isVisible = true,
+                onClick = {
+                    isItemEdit = false
+                    hubDetailsAction(HubDetailsAction.UpdateCurrentItem(null))
+                    itemSheetState = true
+                },
+                modifier = Modifier
+                    .size(64.dp)
+            )
+        },
+        modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            hubItemsResult?.onSuccess { hubItems ->
+                LazyColumn(modifier = Modifier.fillMaxSize()) {
+                    items(
+                        items = hubItems,
+                        key = { hubItem -> hubItem.id }
+                    ) { hubItem ->
+                        ItemListCard(
+                            hubItem = hubItem.toHubItemUI(),
+                            onClick = {
+                                isItemEdit = true
+                                hubDetailsAction(HubDetailsAction.UpdateCurrentItem(hubItem))
+                                itemSheetState = true
+                            },
+                            modifier = Modifier
+                                .padding(4.dp)
+                        )
+                    }
+                }
+            }
+
+            if (hubSheetState) {
+                hubDetailsState.hub?.let { hub ->
+                    HubBottomSheet(
+                        hub = hub.toHubUI(),
+                        sheetState = sheetState,
+                        onDismiss = { hubSheetState = false },
+                        isEdit = true,
+                        onEdit = { hubName, hubDescription ->
+                            baseAction(BaseAction.ShowLoading)
+                            hubDetailsAction(HubDetailsAction.UpdateHub(hubName, hubDescription))
+                        },
+                        onDelete = { hubId ->
+                            baseAction(BaseAction.ShowLoading)
+                            hubDetailsAction(HubDetailsAction.DeleteHub(hubId))
+                        }
                     )
                 }
             }
-        }
 
-        if (hubBottomSheetState) {
-            hubDetailsState.hub?.let { hub ->
-                HubBottomSheet(
-                    hub = hub.toHubUI(),
+            if (itemSheetState) {
+                ItemBottomSheet(
                     sheetState = sheetState,
-                    onDismiss = { onSheetDismiss() },
-                    isEdit = true,
-                    onEdit = { hubName, hubDescription ->
-                        baseAction(BaseAction.ShowLoading)
-                        hubDetailsAction(HubDetailsAction.UpdateHub(hubName, hubDescription))
+                    isEdit = isItemEdit,
+                    hubItem = hubDetailsState.currentItem?.toHubItemUI(),
+                    onDismiss = {
+                        itemSheetState = false
+                        isItemEdit = false
+                        hubDetailsAction(HubDetailsAction.UpdateCurrentItem(null))
                     },
-                    onDelete = { hubId ->
+                    onAdd = { itemName, itemStock, unit ->
                         baseAction(BaseAction.ShowLoading)
-                        hubDetailsAction(HubDetailsAction.DeleteHub(hubId))
+                        hubDetailsAction(
+                            HubDetailsAction.AddItem(
+                                itemName, itemStock.toFloat(), unit
+                            )
+                        )
+                    },
+                    onEdit = { itemId, itemName, itemStock, unit ->
+                        baseAction(BaseAction.ShowLoading)
+                        hubDetailsAction(
+                            HubDetailsAction.UpdateItem(
+                                itemId, itemName, itemStock.toFloat(), unit
+                            )
+                        )
+                    },
+                    onDelete = { itemId ->
+                        baseAction(BaseAction.ShowLoading)
+                        hubDetailsAction(HubDetailsAction.DeleteItem(itemId))
                     }
                 )
             }
-        }
-
-        if (itemBottomSheetState) {
-            ItemBottomSheet(
-                sheetState = sheetState,
-                isEdit = isItemEdit,
-                hubItem = hubDetailsState.currentItem?.toHubItemUI(),
-                onDismiss = {
-                    onSheetDismiss()
-                    isItemEdit = false
-                    hubDetailsAction(HubDetailsAction.UpdateCurrentItem(null))
-                },
-                onAdd = { itemName, itemStock, unit ->
-                    baseAction(BaseAction.ShowLoading)
-                    hubDetailsAction(
-                        HubDetailsAction.AddItem(
-                            itemName, itemStock.toFloat(), unit
-                        )
-                    )
-                },
-                onEdit = { itemId, itemName, itemStock, unit ->
-                    baseAction(BaseAction.ShowLoading)
-                    hubDetailsAction(
-                        HubDetailsAction.UpdateItem(
-                            itemId, itemName, itemStock.toFloat(), unit
-                        )
-                    )
-                },
-                onDelete = { itemId ->
-                    baseAction(BaseAction.ShowLoading)
-                    hubDetailsAction(HubDetailsAction.DeleteItem(itemId))
-                }
-            )
         }
     }
 }
@@ -263,11 +297,6 @@ private fun HubDetailsContentPreview() {
         hubDetailsState = HubDetailsState(),
         hubDetailsAction = {  },
         baseAction = {  },
-        hubBottomSheetState = false,
-        onSheetDismiss = {  },
-        itemBottomSheetState = false,
-        onHubDeletion = {  },
-        onEditItem = {  },
         navigateBack = {  }
     )
 }
