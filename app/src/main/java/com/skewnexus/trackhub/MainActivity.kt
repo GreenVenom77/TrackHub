aeb62b61d87eff3e26e4766636e539468d1925e1
GreenVenom77 <hosome2222@gmail.com>
2025-10-26 18:39:09 +0300

    Refactor: Change theme preference retrieval to a suspend function
    This commit refactors the theme preference retrieval mechanism to address a UI flicker issue on app startup. The `getThemePreference` function in `AppPrefsDataSource` and its implementation `DataStoreAppPrefs` has been changed from returning a `Flow` to a `suspend` function.
    
    ### Key Changes:
    
    *   **`AppPrefsDataSource` & `DataStoreAppPrefs`:**
        *   Modified `getThemePreference` to be a `suspend` function that directly updates the `_appPrefsState` with the theme preference fetched once using `.first()`.
        *   This ensures the theme is loaded before the initial UI composition.
    
    *   **`MainActivity`:**
        *   A `LaunchedEffect` is now used to call the new `getThemePreference` suspend function once when the activity is created.
        *   This change prevents the app from momentarily showing the wrong theme before the preference is collected from the `Flow`.

--- app/src/main/java/com/skewnexus/trackhub/MainActivity.kt
+++ app/src/main/java/com/skewnexus/trackhub/MainActivity.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.greenvenom.core_navigation.data.repository.NavigationStateRepository
 import com.greenvenom.core_ui.components.TopAppBar
 import com.greenvenom.core_ui.presentation.ScaffoldViewModel
@@ -22,6 +24,7 @@ import com.seravian.core_local.domain.AppPrefsDataSource
 import com.skewnexus.trackhub.navigation.AppNavHost
 import com.trackhub.feat_navigation.components.BottomNavigationBar
 import com.trackhub.feat_navigation.routes.Screen
+import kotlinx.coroutines.launch
 import org.koin.compose.koinInject
 
 class MainActivity : AppCompatActivity() {
@@ -36,6 +39,12 @@ class MainActivity : AppCompatActivity() {
             val navigationState by navigationRepository.navigationState.collectAsStateWithLifecycle()
             val scaffoldViewModel: ScaffoldViewModel = koinInject()
             val scaffoldState by scaffoldViewModel.scaffoldState.collectAsStateWithLifecycle()
+
+            LaunchedEffect(Unit)  {
+                lifecycleScope.launch {
+                    appPrefsRepository.getThemePreference()
+                }
+            }
 
             CompositionLocalProvider(LocalScaffoldViewModel provides scaffoldViewModel) {
                 AppTheme(darkTheme = appPrefState.isDarkTheme ?: isSystemInDarkTheme()) {
